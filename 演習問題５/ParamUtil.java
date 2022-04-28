fe6b1297f13737f5aadab0dcc307966aff362f68
ysk <22higa_yuusuke@axiz.co.jp>
2022-04-28 17:34:28 +0900

    演習問題５

--- 演習問題５/ParamUtil.java
+++ 演習問題５/ParamUtil.java
@@ -2,22 +2,35 @@ package util;
 
 /**
  * メソッドをまとめたParamUtilクラス
- */
-public class ParamUtil {
-
+ *	   
     /**
      * ポイント計算
      */
-
+			public class ParamUtil {
+				   public static int getPoint(int amountInt) {
+					    
+					   
+					int Point1 =  (int)(amountInt * 0.01);
+					return Point1;
+				   }
 
     /**
      * ポイント計算(ランクあり)
      */
-
+				   public static int getPoint(int amountInt,double decimal) {
+					    
+					   int Point2 = (int)(amountInt * decimal);
+					   return Point2;
+						
+					   }
 
     /**
      * 引数に指定した文字列がnull、または空文字かを判定
-     */
-
-
+     */ public static boolean isNullOrEmpty(String str) {
+			if(str ==  null || str.isEmpty()) {
+				return true;
+		} else {
+			return false;
+		  }
+     }
 }
